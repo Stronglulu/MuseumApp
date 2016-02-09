a988aade586df37baff4cc528dcf26038e30af76
Wouter Florijn <woutermflorijn@gmail.com>
2016-02-09 20:30:25 +0100

    Automatic painting loading based on file name.

--- Assets/Scripts/Painting.cs
+++ Assets/Scripts/Painting.cs
@@ -3,10 +3,19 @@ using System.Collections;
 
 public class Painting : MonoBehaviour
 {
+    public string paintingName = "";
+
 	void Start()
-	{
-        // Get the width and height of the painting texture.
+    {
         Renderer renderer = GetComponent<Renderer>();
+
+        if (paintingName != "")
+        {
+            Material material = Resources.Load("Materials/Paintings/" + paintingName, typeof(Material)) as Material;
+            renderer.material = material;
+        }
+
+        // Get the width and height of the painting texture.
         Texture texture = renderer.material.mainTexture;
         float paintingWidth = (float)texture.width;
         float paintingHeight = (float)texture.height;
@@ -15,15 +24,9 @@ public class Painting : MonoBehaviour
 
         // Scale the object according to the texture proportions.
         if (proportions > 1)
-        {
-            // Width larger than height.
             transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y / proportions, transform.localScale.z);
-        }
         else
-        {
-            // Height larger than width.
             transform.localScale = new Vector3(transform.localScale.x * proportions, transform.localScale.y, transform.localScale.z);
-        }
     }
 	
 	void Update()
