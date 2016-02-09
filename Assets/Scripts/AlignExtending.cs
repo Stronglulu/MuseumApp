5642973a40a44f97f8643750dcc845788efa101f
Wouter Florijn <woutermflorijn@gmail.com>
2016-02-09 21:19:47 +0100

    Extending effect that automatically scales to match the size of the painting.

---
+++ Assets/Scripts/AlignExtending.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class AlignExtending : MonoBehaviour
+{
+    public Painting painting;
+    public float s = 720;
+
+    private Renderer renderer;
+
+	void Start()
+    {
+        renderer = GetComponent<Renderer>();
+
+        LoadMaterial();
+	}
+	
+	void Update()
+    {
+        ScaleTexture();
+	}
+
+    void LoadMaterial()
+    {
+        string paintingName = painting.paintingName;
+
+        if (paintingName != "")
+        {
+            Material material = Resources.Load("Materials/Illusions/Extending/" + paintingName, typeof(Material)) as Material;
+            renderer.material = material;
+        }
+    }
+
+    void ScaleTexture()
+    {
+        Vector3 wallSize = renderer.bounds.size;
+        float ratioX = (float)renderer.material.mainTexture.width / wallSize.x;
+        float ratioY = (float)renderer.material.mainTexture.height / wallSize.y;
+        Debug.Log("wallSize: " + wallSize);
+        Debug.Log("ratioX: " + ratioX);
+        Debug.Log("ratioY: " + ratioY);
+
+        Vector3 paintingSize = painting.renderer.bounds.size;
+        float maxSize = Math.Max(paintingSize.x, paintingSize.y);
+        float paintingRatio = s / maxSize;
+        Debug.Log("paintingRatio: " + paintingRatio);
+
+        float xs = paintingRatio / ratioX;
+        float ys = paintingRatio / ratioY;
+        Debug.Log("xs: " + xs);
+        Debug.Log("ys: " + ys);
+
+        renderer.material.mainTextureScale = new Vector2(xs, ys);
+        renderer.material.mainTextureOffset = new Vector2(-xs / 2 + 0.5f, -ys / 2 + 0.5f);
+    }
+}
--- Assets/Scripts/Painting.cs
+++ Assets/Scripts/Painting.cs
@@ -4,32 +4,39 @@ using System.Collections;
 public class Painting : MonoBehaviour
 {
     public string paintingName = "";
+    public Renderer renderer;
 
 	void Start()
     {
-        Renderer renderer = GetComponent<Renderer>();
+        renderer = GetComponent<Renderer>();
 
+        LoadMaterial();
+        ScaleObject();
+    }
+
+    // Loads the painting material.
+    void LoadMaterial()
+    {
         if (paintingName != "")
         {
             Material material = Resources.Load("Materials/Paintings/" + paintingName, typeof(Material)) as Material;
             renderer.material = material;
         }
+    }
 
-        // Get the width and height of the painting texture.
+    // Scales the object according to the painting proportions.
+    void ScaleObject()
+    {
+        // Get the proportions of the painting texture.
         Texture texture = renderer.material.mainTexture;
         float paintingWidth = (float)texture.width;
         float paintingHeight = (float)texture.height;
-
         float proportions = paintingWidth / paintingHeight;
 
-        // Scale the object according to the texture proportions.
+        // Scale the object.
         if (proportions > 1)
             transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y / proportions, transform.localScale.z);
         else
             transform.localScale = new Vector3(transform.localScale.x * proportions, transform.localScale.y, transform.localScale.z);
     }
-	
-	void Update()
-	{
-	}
 }
