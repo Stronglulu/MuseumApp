9fb2a2c05b0c6841118e9f2b200882e4ee043531
B-deBoer <b_de_b_@hotmail.com>
2016-02-14 02:04:35 +0100

    Start of hallway
    copied stuff from Xhi Jia and fixed camera direction. Not functional
    yet.

---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProgressBar : MonoBehaviour {
+
+    //private CardboardHead head;
+    private Camera camera;
+    float timer, scale;
+	
+	// Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        //bool isLookedAt = GetComponent<Collider>().Raycast(head.Gaze, out hit, Mathf.Infinity);
+        bool isLookedAt = GetComponent<Collider>().Raycast(new Ray(camera.transform.position, camera.transform.forward), out hit, Mathf.Infinity);
+        if (isLookedAt)
+        {
+
+
+            timer += Time.deltaTime;
+            scale = 5 / 3 * timer;
+            this.transform.localScale = new Vector3(scale, 0.01f, 1);
+            //if (timer > 3)
+            // Application.LoadLevel("basicRoom");
+        }
+        else
+        {
+            //this.GetComponent<SpriteRenderer>().color = Color.white;
+            this.transform.localScale = new Vector3(0, 0.01f, 1);
+            timer = 0;
+        }
+    }
+}
