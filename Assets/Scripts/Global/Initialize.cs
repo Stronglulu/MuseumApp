332bc61ee0d4c3b4433458c5f7a5a11378c49e04
XhiJia <xhijia_tan@hotmail.com>
2016-02-23 00:30:14 +0100

    Automatically creates new log file

--- Assets/Scripts/Global/Initialize.cs
+++ Assets/Scripts/Global/Initialize.cs
@@ -1,13 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class Initialize : MonoBehaviour {
+
+    string logPath = "/log1.csv";
+    string dataPath;
+    int logNr = 1;
 
 	// Use this for initialization
 	void Start()
     {
-        Museum.Load("/setup.txt", "/log.csv");
-        SceneManager.LoadScene("Scenes/Hallway");
+        dataPath = Museum.getDataPath();
 	}
+
+    void Update()
+    {
+        // Loop through existing files until one cannot be found.
+        while (File.Exists(dataPath + logPath))
+        {
+            logNr++;
+            logPath = "/log" + (logNr) + ".csv";
+            Debug.Log(dataPath + logPath);
+        }
+
+        Museum.Load("/setup.txt", logPath);
+        SceneManager.LoadScene("Scenes/Hallway");
+    }
 }
