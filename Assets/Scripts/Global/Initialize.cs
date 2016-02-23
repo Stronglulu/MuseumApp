c010edfd3d61119c4da03a8dca0a72fb50ecf639
Wouter Florijn <woutermflorijn@gmail.com>
2016-02-23 14:51:32 +0100

    Cleaned up initialization.

--- Assets/Scripts/Global/Initialize.cs
+++ Assets/Scripts/Global/Initialize.cs
@@ -3,20 +3,16 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 using System.IO;
 
-public class Initialize : MonoBehaviour {
-
+public class Initialize : MonoBehaviour
+{
     string logPath = "/log1.csv";
     string dataPath;
     int logNr = 1;
 
-	// Use this for initialization
 	void Start()
     {
         dataPath = Museum.getDataPath();
-	}
 
-    void Update()
-    {
         // Loop through existing files until one cannot be found.
         while (File.Exists(dataPath + logPath))
         {
