c52f3dbdf2102bbd25a2815156a50dda0cbfd8f7
XhiJia <xhijia_tan@hotmail.com>
2016-05-11 15:30:23 +0200

    Added plants in the scene

---
+++ Assets/Scripts/Decoration.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Decoration : MonoBehaviour {
+
+    GameObject child;
+
+	// Use this for initialization
+	void Start () {
+
+        try
+        {
+            child = transform.FindChild("Floor" + (Museum.currentFloor)).gameObject;
+        }
+        catch (Exception e)
+        {
+            print("no child found");
+        }
+        if(child != null)
+            child.SetActive(true);
+        
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
