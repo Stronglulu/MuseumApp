994f20c33b5154d540a8abeb6ed80af6ad8cdf0d
mrgurdi <mod_ferdi@hotmail.com>
2016-06-02 11:20:17 +0200

    Added swap from museum room to scene

---
+++ Assets/Scripts/SceneSwapChecker.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneSwapChecker : MonoBehaviour
+{
+    public Transform Transformor;
+    public int ThresholdZ;
+    public GameObject MuseumRoom;
+    public GameObject PaintingScene;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Transformor.position.z > ThresholdZ)
+	    {
+	        if (!PaintingScene.activeSelf)
+	        {
+	            PaintingScene.SetActive(true);
+	            MuseumRoom.SetActive(false);
+	            print("Sakura");
+	        }
+	    }
+	    else
+	    {
+	        if (!MuseumRoom.activeSelf)
+	        {
+	            PaintingScene.SetActive(false);
+	            MuseumRoom.SetActive(true);
+	            print("Museum");
+	        }
+	    }
+	}
+}
