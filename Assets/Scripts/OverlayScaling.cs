61b08f874fc8e6318bb86cebd8cd8b2ecded0dd4
XhiJia <xhijia_tan@hotmail.com>
2016-06-03 16:58:11 +0200

    Added scaling extending illusion (1st version)

---
+++ Assets/Scripts/OverlayScaling.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OverlayScaling : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Renderer rend = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (transform.localScale.x < 7.3f)
+        {
+            transform.localScale = new Vector3(transform.localScale.x + 0.01f, transform.localScale.y, transform.localScale.z + 0.01f);
+        }
+	}
+}
