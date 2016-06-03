3a7281e0cf37f7b54941bd42e78b7f8bfc976147
XhiJia <xhijia_tan@hotmail.com>
2016-06-04 00:52:49 +0200

    Stylized Painting

---
+++ Assets/OverlayTransitionDistance.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OverlayTransitionDistance : MonoBehaviour
+{
+    public GameObject player;
+    float maxDistance = 4;
+    float distance;
+    Renderer rend;
+    bool increase = false;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        distance = Vector3.Distance(player.transform.position, transform.position);
+        Debug.Log(rend.material.GetFloat("_Blend"));
+        if (distance < maxDistance)
+        {
+            if (increase)
+                rend.material.SetFloat("_Blend", rend.material.GetFloat("_Blend") + 0.01f);
+            else
+                rend.material.SetFloat("_Blend", rend.material.GetFloat("_Blend") - 0.01f);
+
+            if (rend.material.GetFloat("_Blend") >= 1 || rend.material.GetFloat("_Blend") <= 0)
+                increase = !increase;
+        }
+    }
+}
