8cab3c48347d852aa52b554d05249e95b85a61ec
mrgurdi <mod_ferdi@hotmail.com>
2016-06-03 17:30:43 +0200

    Added Weather Effect Checker

---
+++ Assets/Scripts/WeatherEffectChecker.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeatherEffectChecker : MonoBehaviour
+{
+    public Transform PlayerTransform;
+    public int SpawnRadius;
+    public ParticleSystem WeatherParticleSystem;
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    float paintingDistance = Vector3.Distance(PlayerTransform.position, transform.position);
+
+	    if (paintingDistance < SpawnRadius)
+	    {
+	        if (!WeatherParticleSystem.isPlaying)
+	        {
+	            WeatherParticleSystem.Play();
+	        }
+
+	    }
+	    else
+	    {
+            if (WeatherParticleSystem.isPlaying)
+            {
+                WeatherParticleSystem.Stop();
+            }
+	    }
+
+	}
+}
